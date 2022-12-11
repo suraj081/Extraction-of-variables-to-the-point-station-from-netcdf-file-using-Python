f972510b681f3ac5fc40f3fbce25afc88b62e2c7
suraj081 <42827827+suraj081@users.noreply.github.com>
2022-12-11 10:26:08 +0545

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,19 @@
 import netCDF4
 from scipy.interpolate import interpn
+## open text file containing the latitiude and longitude of the stations
 file1=open(r"I:\data-precipitation\4-datasets\cordinates-station.txt")
 for count,line in enumerate(file1):
 	if count!=0:
+		## iterate over all the time series netcdf file
 		for file in glob.glob(r"I:\data-precipitation\4-datasets\cmorph\*.nc"):
 			nc_file = netCDF4.Dataset(file,'r')
 			print(file)
-
+			## cmorph is the variable we are interested here, but can be anything like temperature
 			temp = nc_file.variables['cmorph']
 			# print(temp)
 			lat = nc_file.variables['lat']
 			lon = nc_file.variables['lon']
-		# Define the point for which the data value is to be estimated
+		# Define the point for which the data value is to be estimated, these assigns your location of the point of interest
 			point_lat = line.split('\t')[1]
 			point_lon = line.split('\t')[2]
 			# Define the coordinates of the data grid
@@ -22,10 +24,10 @@ for count,line in enumerate(file1):
 			# Define the data values of the data grid
 			temps = temp[0,:,:]
 			# print(temps)
-			# Estimate the temperature at the specified point using linear interpolation
+			#since our point may not intersect at center of the grid exactly, so we did linear interpolationinterploate to derive the exact values
 			point_temp = interpn((lats, lons), temps, (point_lat, point_lon), method='linear')
 
 			# # Print the estimated temperature at the point
-			# print(point_temp)
+			# print(point_temp) this allows the time series value for single point and accordingly
 			with open(r"I:\data-precipitation\4-datasets\cmorph\%s.txt"%(count),'a+') as f1:
 				f1.write(str(round(point_temp[0],3))+'\n')
